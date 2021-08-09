6c7f9b019b1ad08456f8a81c0f2efff7adfa60e7
Monique <80782598+monicaardila@users.noreply.github.com>
2021-08-09 18:40:21 -0500

    Add files via upload

---
+++ tarjeta_edwin_javascript-master/js/efecto.js
@@ -0,0 +1,12 @@
+function changeColor(){
+    let color = document.getElementById("colorText").value;
+    document.getElementById("card2").style.backgroundColor = color;
+}
+
+function border(){
+    let bord = document.getElementById("cardBorder").value;
+    document.getElementById("cardBorder").style.borderRadius = "15px";
+}
+
+
+
